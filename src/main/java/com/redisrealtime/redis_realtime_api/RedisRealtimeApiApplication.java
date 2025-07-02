46f0794f4d17d7f9b3ec8cae09450db1545e420f
Wally <1506325470piao@gmail.com>
2025-07-03 02:09:35 +0900

    chore: initialize Spring Boot project for redis-realtime-api

---
+++ src/main/java/com/redisrealtime/redis_realtime_api/RedisRealtimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.redisrealtime.redis_realtime_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisRealtimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisRealtimeApiApplication.class, args);
+	}
+
+}
